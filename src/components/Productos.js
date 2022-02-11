e6e03ad022a49428a07cc8a4b7cd65e2da98f93b
alejandropl07 <aprietoleon97@gmail.com>
2022-02-11 16:52:02 -0600

    API creada con JSON Server

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-const Productos    =   ()  =>  {
-    return(
-        <p>productos</p>
-    );
-}
+const Productos = () => {
+  return (
+    <React.Fragment>
+      <h2 className="text-center my-5">Listado de Productos</h2>
 
-export default  Productos;
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </React.Fragment>
+  );
+};
+
+export default Productos;
