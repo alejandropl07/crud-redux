4f0509ef2bf8cba5e8d8f1932b664529e86bea0b
alejandropl07 <aprietoleon97@gmail.com>
2022-02-10 21:10:41 -0600

    Store creado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import react from "react";
 
+import store from "./store";
+import { Provider } from "react-redux";
+
 function App() {
   return (
+    <Provider store={store}>
     <div >
       <p>Crud con redux</p>
       </div>
+      </Provider>
   );
 }
 
